0dbf60f451e89f9b51481c5ec7577e812f0fc6dd
Affan Ahmed Khan <affan2k3@gmail.com>
2024-03-27 03:10:06 +0500

    exercises of filtering data

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const Course = mongoose.model('Course', courseSchema)
 async function createCourse() {
 const course = new Course({
     name: "NextJS",
-    author: "Affan",
+    author: "Affan Ahmed",
     tags: ['node', 'Frontend'],
     isPublished: true,
 })
@@ -31,19 +31,47 @@ const course = new Course({
     console.log(result)
 }
 
+
+// createCourse()
+
+
+
+
 async function getCourses() {
+    const pageNumber = 4;
+    const pageSize = 1
     const course = await Course
-        // .find({ author: "Affan", tags: ['node', "Frontend"] })
+        
+    
+        
+        //   STARTS WITH 
+        // .find({ author: /Affan/ })
+ 
+        //   ENDS WITH 
+        // .find({ author: /Ahmed$/i })
+        
+        
+        //   INCLUDE
+        // .find({ author: /.*ahmed.*/i })
+
+        
+        
+        // .find({ author: "Affan" })
         // .find({price: { $gte : 10, $lte: 20}})
         // .find({price:{ $in:[10,15,20]}})
-        .find()
-        .and([{author :"Affan"}, { isPublished:true}])
-        .limit(10) 
+        // .and([{author :"Affan"}, { isPublished:true}])
+        
+        
+        .find({})
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize) 
         .sort({ date: 1 })
         // .select({ name: 1, tags: 1 })
+    // .count()
     console.log(course)
 }
 
 
 
 getCourses()
+
